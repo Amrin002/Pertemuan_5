a0802bb719b8e14b093f9e2af523ea3ebda57c41
Amrin02 <amrinlatuconsina@gmail.com>
2024-05-03 08:51:47 +0700

    add database

---
+++ app/Http/Controllers/PegawaiController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\pegawai;
+use Illuminate\Http\Request;
+
+class PegawaiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $pegawai = pegawai::all();
+        return view('pegawai.index',compact('pegawai'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+        return view('pegawai.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'nama' => 'required|max:255',
+            'pangkat' => 'required|in:juru,pengatur,penata',
+            'alamat' => 'required',
+        ]);
+        $pegawai = pegawai::create($validateData);
+        return redirect('/pegawai')
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(pegawai $pegawai)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(pegawai $pegawai)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, pegawai $pegawai)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(pegawai $pegawai)
+    {
+        //
+    }
+}
